8a0c42ebf91e7093345b0d0adbb6380058ad600e
JatoMixo <jatomixo@outlook.es>
2022-12-02 16:19:17 +0100

    main

--- Day2/main.cpp
+++ Day2/main.cpp
@@ -1,19 +1,31 @@
 #include <iostream>
 
-//TODO Input system
+//TODO Input System
 //TODO Make Equivalent
 //TODO Calculate score
 //TODO Give score / output
 
 void inputMove(std::string message, char *var);
+void enterMoves(char *opponentMoves, char *playerMoves);
 
 int main(){
     char opponentMoves[3];
     char playerMoves[3];
 
-    inputMove("Enter opponent's first move: ", &opponentMoves[0]);
+    enterMoves(opponentMoves, playerMoves);
 
     return 0;
+}
+
+void enterMoves(char *opponentMoves, char *playerMoves){
+    int len = sizeof(*opponentMoves)/sizeof(opponentMoves[0]);
+
+    inputMove("Enter opponent's first move: ", &opponentMoves[0]);
+    inputMove("Enter opponent's second move: ", &opponentMoves[1]);
+    inputMove("Enter opponent's third move: ", &opponentMoves[2]);
+    inputMove("Enter player's first move: ", &opponentMoves[0]);
+    inputMove("Enter player's second move: ", &opponentMoves[1]);
+    inputMove("Enter player's third move: ", &opponentMoves[2]);
 }
 
 void inputMove(std::string message, char *var){
