64d2ad82a0f0cf64aa204ea4fcca0c3cfd7af1a8
JatoMixo <jatomixo@outlook.es>
2022-12-02 17:04:19 +0100

    main

--- Day2/main.cpp
+++ Day2/main.cpp
@@ -1,31 +1,48 @@
 #include <iostream>
 
 //UPGRADE Input System
-//TODO Make Equivalent
 //TODO Calculate score
 //TODO Give score / output
 
 void inputMove(std::string message, char *var);
 void enterMoves(char *opponentMoves, char *playerMoves);
-void translate(char *opponentMoves, char *playerMoves);
+void calculateScores(char *opponentMoves, char *playerMoves);
 
 int main(){
+    // Moves every player will do
     char opponentMoves[3];
     char playerMoves[3];
 
+    // Input all moves
     enterMoves(opponentMoves, playerMoves);
+    
+    // Calculate scores
+    int opponentScore;
+    int playerScore;
+
+    calculateScores(opponentMoves, playerMoves);
 
     return 0;
 }
 
-void translate(char *opponentMoves, char *playerMoves){
+// Calculate the player's scores
+void calculateScores(char *opponentMoves, char *playerMoves){
+    int opponentScore;
+    int playerScore;
+
     for (int i = 0; i < sizeof(*opponentMoves)/sizeof(opponentMoves[0]); i++){
-        if (opponentMoves[i] == 'A'){
-            
-        }
+        opponentScore += opponentMoves[i] % 64;
+        playerScore += playerMoves[i] % 87;
+
+        if (opponentMoves[i] == playerMoves[i]){
+            playerScore += 3;
+            opponentScore += 3;
+            continue;
+        } 
     }
 }
 
+// Enter all moves and put them in the arrays
 void enterMoves(char *opponentMoves, char *playerMoves){
     int len = sizeof(*opponentMoves)/sizeof(opponentMoves[0]);
 
@@ -37,6 +54,7 @@ void enterMoves(char *opponentMoves, char *playerMoves){
     inputMove("Enter player's third move: ", &opponentMoves[2]);
 }
 
+// Input a single move of a player
 void inputMove(std::string message, char *var){
     std::cout << message;
     char out;
