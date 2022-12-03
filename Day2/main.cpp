f94620fd4a8949205b6b0a433859875edd41886f
JatoMixo <jatomixo@outlook.es>
2022-12-03 13:27:52 +0100

    main

--- Day2/main.cpp
+++ Day2/main.cpp
@@ -1,9 +1,5 @@
 #include <iostream>
 
-//UPGRADE Input System
-//TODO Calculate score
-//TODO Give score / output
-
 void inputMove(std::string message, char *var);
 void enterMoves(char *opponentMoves, char *playerMoves);
 int calculateScore(char *opponentMoves, char *playerMoves);
@@ -13,28 +9,35 @@ int main(){
     char opponentMoves[3];
     char playerMoves[3];
 
+    // Explaining text
+    std::cout << "Player moves: Rock(X), Paper(Y) and Scissors(Z)\n";
+    std::cout << "Opponent moves: Rock(A), Paper(B) and Scissors(C)\n";
+    std::cout << "--------------------------------------------------\n";
+
     // Input all moves
     enterMoves(opponentMoves, playerMoves);
     
     // Calculate scores
-    std::cout << "Your score is: " << calculateScore(opponentMoves, playerMoves);
+    std::cout << "Your score is: " << calculateScore(opponentMoves, playerMoves) << "\n";
 
     return 0;
 }
 
-// Calculate the player's scores
+// Calculate player's score
 int calculateScore(char *opponentMoves, char *playerMoves){
-    int playerScore;
+    int playerScore = 0;
+    int len = 3;
 
-    for (int i = 0; i < sizeof(*opponentMoves)/sizeof(opponentMoves[0]); i++){
-        playerScore += playerMoves[i] % 87;
+    // Iterate over all of 3 moves
+    for (int i = 0; i < len; i++){
+        playerScore += (int)(playerMoves[i] % 87);
 
-        if (opponentMoves[i] == playerMoves[i]){
+        if (opponentMoves[i] % 64 == playerMoves[i] % 87){ // If it's a draw
             playerScore += 3;
             continue;
-        } else if (playerMoves[i] % 87 == 2 && opponentMoves[i] % 64 == 0){
+        } else if (playerMoves[i] % 87 == 2 && opponentMoves[i] % 64 == 0){ // If the opponent wins because he throw Rock and us Scissors
             continue;
-        } else if (playerMoves[i] % 87 > opponentMoves[i] % 64){
+        } else if (playerMoves[i] % 87 > opponentMoves[i] % 64){ // If we get a higher number, we will win, else not.
             playerScore += 6;
         } 
     }
@@ -44,14 +47,13 @@ int calculateScore(char *opponentMoves, char *playerMoves){
 
 // Enter all moves and put them in the arrays
 void enterMoves(char *opponentMoves, char *playerMoves){
-    int len = sizeof(*opponentMoves)/sizeof(opponentMoves[0]);
+    int len = 3;
 
-    inputMove("Enter opponent's first move: ", &opponentMoves[0]);
-    inputMove("Enter opponent's second move: ", &opponentMoves[1]);
-    inputMove("Enter opponent's third move: ", &opponentMoves[2]);
-    inputMove("Enter player's first move: ", &opponentMoves[0]);
-    inputMove("Enter player's second move: ", &opponentMoves[1]);
-    inputMove("Enter player's third move: ", &opponentMoves[2]);
+    // Input all moves for the player and the opponent
+    for (int i = 0; i < len; i++){
+        inputMove("Enter opponent's move: ", &opponentMoves[i]);
+        inputMove("Enter player's move: ", &playerMoves[i]);
+    }
 }
 
 // Input a single move of a player
